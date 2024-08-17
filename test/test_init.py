6d1a64f7fb71380122d1c9711699c0838ae6ea96
bobrobotsun <bobrobotsun@163.com>
2024-08-18 01:13:29 +0800

    2024.08.17.02-movoid 新增测试函数 补充gitignore

---
+++ test/test_init.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : test_init
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/8/18 1:08
+# Description   : 
+"""
+from RobotFrameworkRequests import RobotFrameworkRequests, RobotRequestsBasic
+
+
+class Test_init:
+    def test_init(self):
+        rfr = RobotFrameworkRequests()
+        rrb = RobotRequestsBasic()
